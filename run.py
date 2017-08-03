d358d43308a6c78a3ba6225ceec26e13f389274a
richardshi0119 <richardshi0119@gmail.com>
2017-08-03 22:45:12 +0800

    uband-s2-developers

--- run.py
+++ run.py
@@ -8,9 +8,9 @@ def hello_world():
 	print "hello"
 	return render_template('index.html')
 
-@app.route('/B19724')
-def details():
-	return render_template('B19724.html')
+@app.route('/<string:student_number>/details')
+def details(student_number):
+	return render_template(str(student_number) + '.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
